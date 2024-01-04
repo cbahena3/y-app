57a71a3cf8e4932d348b205e700f16033e112e39
Cristian B <cris.bahena77@gmail.com>
2024-01-04 14:11:37 -0600

    Added show action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,6 +9,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_equal Photo.count, data.length
   # end
 
+  # test "show" do
+  #   get "/photos/#{Photo.first.id}.json"
+  #   assert_response 200
+
+  #   data = JSON.parse(response.body)
+  #   assert_equal ["id", "name", "width", "height", "created_at", "updated_at"], data.keys
+  # end
+
   # test "create" do
   #   assert_difference "User.count", 1 do
   #     post "/users.json", params: { name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
